5b6d63e26ebc64c3254018c0b122e5eae5e3af95
nybakk1 <nybakk1@hotmail.com>
2016-09-15 16:29:45 +0200

    Update Hitra.java

--- Hitra.java
+++ Hitra.java
@@ -15,32 +15,64 @@
 import lejos.hardware.motor.*;
 import lejos.hardware.lcd.*;
 import lejos.hardware.sensor.EV3TouchSensor;
-import lejos.hardware.sensor.EV3Colorsensor;
+import lejos.hardware.sensor.EV3UltrasonicSensor;
+import lejos.hardware.sensor.EV3ColorSensor;
+import lejos.hardware.sensor.NXTSoundSensor;
+import lejos.hardware.sensor.*;
+import lejos.hardware.ev3.EV3;
 import lejos.hardware.port.Port;
 import lejos.hardware.Brick;
 import lejos.hardware.BrickFinder;
+import lejos.hardware.Keys;
+import lejos.hardware.sensor.SensorModes;
 import lejos.robotics.SampleProvider;
+import lejos.hardware.sensor.*;
 
-public class Hitra {
-  public static void main(String[] args) {
 
-    //Definerer Brick.sensorer
+public class Golfbane {
+public static void main (String[] args) throws Exception {
+   
+    Brick brick = Brickfinder.getDefault();
+	Motor.A.setSpeed(500);      // setter hastighet til 500
+	Motor.B.setSpeed(500);      // setter hastighet til 500
+	Motor.C.setSpeed(500);       
+	Port s1 = brick.getPort("S1");  // trykksensor 1  Langs bakken
+	Port s2 = brick.getPort("S2");   // trykksensor 2 Oppe på bilen
+	Port s4 = brick.getPort("S4");   // ultrasoniske sensoren   
+	
+	EV3 ev3 = (EV3) Brickfinder.getLocal();
+	TextLCD lcd = ev3.getTextLCD();
+	Keys keys = ev3.getKeys();
+	
+	lcd.drawString("Trykk for å starte", 0, 1);
+	keys.waitForAnyPress();
+	
+  // definerer fargesensor
+  
+   EV3ColorSensor fargesensor = new EV3ColorSensor(s3); // ev3-fargesensor
+   SampleProvider fargeLeser = fargesensor.getMode("RGB");  // svart = 0.01..	
+   float[] fargeSample = new float [fargeLeser.sampleSize()];  // tabell som inneholder
 
-    //Definerer trykksensor
-
-    //Definerer lydsensor
-
-    //Definerer lyssensor
-
-    //Setter hastighet motorer
-    Motor.A.setSpeed(300);
-    Motor.B.setSpeed(300);
-
-    boolean continue = true;
-
-    while (continue) {
-
-    }
-
-  }
-}
+ // definerer trykksensor 1
+ 
+   SampleProvider trykksensor1 = new EV3TouchSensor(s1);
+   float[] trykksample1 = new float[trykksensor1.sampleSize()];       // tabell som inneholder avlest verdi
+   
+ // definerer trykksensor 2
+   
+   SampleProvider trykksensor2 = new EV3TouchSensor(s2);
+   float[] trykksample2 = new float[trykksensor2.sampleSize()];
+   
+   // definerer ultrasensoren
+   EV3UltrasonicSensor ultraSensor = new EV3UltrasonicSensor(s4);
+   SampleProvider ultraLeser = ultraSensor.getDistanceMode();
+   float[] ultraSample = new float[ultraLeser.sampleSize()];    // tabell som inneholder avlest verdi
+   
+   // definerer lydsensor
+   
+   
+   
+   
+  
+ }     // main metode
+}     // class
